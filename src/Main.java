1a02b0ce953d8c6cb5634e34cd9ce24750ab7ea2
Anastasia <vesheneba2020@gmail.comgit config --global user.email vesheneba2020@gmail.com>
2024-06-04 16:12:07 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int sum = 100;
+        int replenishment = 300;
+        int cash = sum + replenishment;
+
+        int bonus = cash / 100;
+
+        if (cash >= 1000) {
+
+            System.out.println(bonus);
+
+    } else {
+            System.out.println(cash);
+        }
+}}
